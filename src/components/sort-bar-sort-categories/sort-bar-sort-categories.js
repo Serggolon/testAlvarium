5c673dc70c8bf172c090c07da0ce44ac5d0dc7f2
Serggolon <35743167+Serggolon@users.noreply.github.com>
2021-02-12 08:27:04 +0200

    Удалена логика обработки атрибута cheked у input

--- src/components/sort-bar-sort-categories/sort-bar-sort-categories.js
+++ src/components/sort-bar-sort-categories/sort-bar-sort-categories.js
@@ -4,41 +4,21 @@ import SortBarComponentName from "../sort-bar_component_name";
 
 import './sort-bar-sort-categories.css';
 
-const SortBarSortCategories = ({sortParameters, onSortChange}) => {
-
-    let asc , desc , alph;
-
-    switch (sortParameters) {
-        case "ascending":
-            asc = true;
-            desc = false;
-            alph = false;
-            break;
-        case "descending":
-            asc = false;
-            desc = true;
-            alph = false;
-            break;
-        case "alphabet":
-            asc = false;
-            desc = false;
-            alph = true;
-            break;
-    }
+const SortBarSortCategories = ({onSortChange}) => {
 
     return (
         <section className={"sort-bar-sort-categories"}>
             <SortBarComponentName name={"Сортировка"}/>
             <article>
-                <input id={"ascending"} name="sort" type="radio" value="ascending" checked={asc}
+                <input id={"ascending"} name="sort" type="radio" value="ascending"
                        onChange={(event) => onSortChange(event.target.value)}/>
                 <label htmlFor="ascending">По возрастанию цены</label>
 
-                <input id={"descending"} name="sort" type="radio" value="descending" checked={desc}
+                <input id={"descending"} name="sort" type="radio" value="descending"
                        onChange={(event) => onSortChange(event.target.value)}/>
                 <label htmlFor="descending">По убыванию цены</label>
 
-                <input id={"alphabet"} name="sort" type="radio" value="alphabet" checked={alph}
+                <input id={"alphabet"} name="sort" type="radio" value="alphabet"
                        onChange={(event) => onSortChange(event.target.value)}/>
                 <label htmlFor="alphabet">По алфавиту</label>
             </article>
